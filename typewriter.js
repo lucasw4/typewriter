dccbf6e0ad9dee5e05a8315094498242e6af814c
Lucas Sahota <lucaswade4@gmail.com>
2022-10-15 20:16:16 -0700

    typewriter assignment for lighthouse labs

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+
+let timeout = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, timeout);
+  timeout += 50;
+}
+setTimeout(() => {
+  console.log();
+}, timeout);
